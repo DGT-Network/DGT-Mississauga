31aed8f0806b6b8fd2050e06740b9a1b390a1915
Sergey Kulikov <kulikov112@users.noreply.github.com>
2018-12-06 14:48:37 +0300

    almost done

--- bgx/dashboard/bjx/front/logic/peers.js
+++ bgx/dashboard/bjx/front/logic/peers.js
@@ -9,7 +9,55 @@ export function convertPeers(data) {
                 size: 4000}
       }),
   }
+  let r = {}
 
-  //console.log('dataPeer',parent_node)
-  return parent_node;
+  let t = convertNode(r, data.data.net_structure.parent_node )
+
+  console.log (t)
+
+  return t
+}
+
+function convertNode(r, node, parent_node = null){
+
+  console.log(node)
+
+  let ch = []
+  let parentRelation = []
+
+  if (parent_node != null)
+    parentRelation = [parent_node.IP]
+
+ if (typeof node.children !== 'undefined'){
+    ch = node.children
+    ch.forEach((j) => {
+          (convertNode(r, j, node));
+        })
+
+
+  }
+
+  r[node.IP] =  {
+      name: node.IP,
+      type: "group0",
+      depends:  ch.map((j) => {
+        return j.IP;
+        }),
+      dependedOnBy: parentRelation,
+    }
+
+  return r;
+
+  // node.children.map((j) => {
+  //       return {IP: j.IP,
+  //               size: 4000}
+  //     })
+  // return {
+  //   "IP": {
+  //     name: node.IP,
+  //     type: "group0",
+  //     depends: [],
+  //     dependedOnBy: [],
+  //   }
+  // }
 }
